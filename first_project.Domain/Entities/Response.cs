edcbcd17d029da059399af3a7836ddf88cfcbed0
Winy81 <Adam.Nemeth@FootballPools.com>
2021-02-03 14:00:38 +0000

    Setup the first route wiht a get request

---
+++ first_project.Domain/Entities/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace first_project.Domain.Entities
+{
+    public class Response
+    {
+        public Response()
+        {
+        }
+    }
+}
---
+++ first_project/Controllers/StreakController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace first_project.API.Controllers
+{
+    public class StreakController
+    {
+        public StreakController()
+        {
+        }
+    }
+}
